bebf7e05fa372981acba3fb5355dda311f60e3ed
Charlie Briggs <charliebriggs@gmail.com>
2021-07-29 23:46:11 +0100

    Extra pyenv init

--- python/env.zsh
+++ python/env.zsh
@@ -7,6 +7,7 @@ if command -v python 1>/dev/null 2>&1; then
 fi
 export PATH=$HOME/.poetry/bin:$PATH
 if command -v pyenv 1>/dev/null 2>&1; then
+	eval "$(pyenv init --path)"
 	eval "$(pyenv init -)"
 	eval "$(pyenv virtualenv-init -)"
 fi
