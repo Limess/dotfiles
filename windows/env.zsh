9f7c65b23dd58538d05c73cd6460f4fe7a9b4d43
Charlie Briggs <charliebriggs@gmail.com>
2021-10-02 13:10:55 +0100

    Use wsl-open on WSL

--- windows/env.zsh
+++ windows/env.zsh
@@ -1,8 +1,7 @@
 export IS_WSL=$(uname -a | grep -i -c "microsoft")
 
 if [[ "${IS_WSL}" -eq 1 ]]; then
-    export BROWSER="/mnt/c/Program\ Files/Mozilla\ Firefox/firefox.exe"
-
-    alias open="$BROWSER $1"
+    	# Use wsl-open on WSL https://github.com/4U6U57/wsl-open
+	alias open="wsl-open $1"
 fi
 
