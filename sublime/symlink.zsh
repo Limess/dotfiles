7c277f087fd427b613f0dd0023ca6d89d259d60f
Charlie Briggs <charliebriggs@gmail.com>
2021-12-05 11:21:34 +0000

    cb: Tweaks

--- sublime/symlink.zsh
+++ sublime/symlink.zsh
@@ -1,5 +1,6 @@
+SOURCE="~/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
 LINK="/usr/local/bin/sublime"
 
-if [ ! -L "$LINK" ]; then
-    sudo ln -sf "~/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" $LINK
+if [ -f "$SOURCE" ] && [ ! -L "$LINK" ]; then
+    sudo ln -sf "$SOURCE" $LINK
 fi
