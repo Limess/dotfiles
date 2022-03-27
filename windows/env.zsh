275a789d2711d3d4676ce31bce8adc7ec643671a
Charlie Briggs <charliebriggs@gmail.com>
2022-03-27 11:16:40 +0100

    cb: Add pkgconfig path

--- windows/env.zsh
+++ windows/env.zsh
@@ -5,19 +5,14 @@ export IS_WSL=$(uname -a | grep -i -c "microsoft")
 if [[ "${IS_WSL}" -eq 1 ]]; then
     	# Use wsl-open on WSL https://github.com/4U6U57/wsl-open
 	alias open="wsl-open $1"
-fi
 
-
-if [ -z "$SSH_TTY" ]; then
-	if [ "${IS_WSL}" -eq 1 ]; then
+    if [ -z "$SSH_TTY" ]; then
 	    # http://manpages.ubuntu.com/manpages/xenial/man1/keychain.1.html
 		# https://devblogs.microsoft.com/commandline/sharing-ssh-keys-between-windows-and-wsl-2/
 		# add id_rsa ssh key to keychain which is shared between tabs and start ssh agent
 		eval `keychain --agents ssh -q --eval id_rsa`
-	fi
-fi
+    fi
 
-if [[ "${IS_WSL}" -eq 1 ]]; then
 	# Simple alternative to background tasks on startup rather than having to run fakesystemd
 	# check with ps first as we need sudo to actually check status
 	dnsmasq_running=$(ps -fC dnsmasq)
@@ -28,4 +23,6 @@ if [[ "${IS_WSL}" -eq 1 ]]; then
 			sudo service dnsmasq restart
 		fi
 	fi
+
+    export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:/usr/lib/x86_64-linux-gnu/pkgconfig"
 fi
