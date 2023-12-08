4ae90850268e42cfb68236de5d50c8c00201f90a
Charlie Briggs <charliebriggs@gmail.com>
2023-12-08 09:26:06 +0000

    Tweaks

--- python/env.zsh
+++ python/env.zsh
@@ -5,7 +5,7 @@ export PATH=$PATH:$(python3 -c "import site; print(site.USER_BASE)")/bin/
 if command -v python 1>/dev/null 2>&1; then
 	export PATH=$PATH:$(python -c "import site; print(site.USER_BASE)")/bin/
 fi
-export PATH=$HOME/.poetry/bin:$PATH
+export PATH=$HOME/.poetry/bin:$HOME/.local/bin:$PATH
 if command -v pyenv 1>/dev/null 2>&1; then
 	eval "$(pyenv init --path)"
 	eval "$(pyenv init -)"
