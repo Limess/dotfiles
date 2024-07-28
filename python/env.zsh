fbf103a4048edeaab7045ab57472534d2ab06471
Charlie Briggs <charliebriggs@gmail.com>
2024-07-28 11:03:12 +0100

    Python tweaks@

--- python/env.zsh
+++ python/env.zsh
@@ -1,17 +1,23 @@
 export PIPENV_VENV_IN_PROJECT=true
+
 export POETRY_VIRTUALENVS_IN_PROJECT=true
+export POETRY_VIRTUALENVS_PREFER_ACTIVE_PYTHON=true
+
 export PIPX_HOME="~/.local/pipx"
 
-export PATH=$PATH:$(python3 -c "import site; print(site.USER_BASE)")/bin/
-if command -v python 1>/dev/null 2>&1; then
-	export PATH=$PATH:$(python -c "import site; print(site.USER_BASE)")/bin/
-fi
 export PATH=$HOME/.poetry/bin:$HOME/.local/bin:$PATH
+
 if command -v pyenv 1>/dev/null 2>&1; then
 	eval "$(pyenv init --path)"
 	eval "$(pyenv init -)"
 	eval "$(pyenv virtualenv-init -)"
 fi
+
+# seems unnecessary these days
+# export PATH=$PATH:$(python3 -c "import site; print(site.USER_BASE)")/bin/
+# if command -v python 1>/dev/null 2>&1; then
+#	export PATH=$PATH:$(python -c "import site; print(site.USER_BASE)")/bin/
+# fi
 
 IS_WSL=$(uname -a | grep -i -c "microsoft")
 
@@ -22,3 +28,4 @@ fi
 
 alias dvc="nocorrect dvc"
 alias gto="nocorrect gto"
+
